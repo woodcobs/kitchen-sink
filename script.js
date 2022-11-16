511fcd7ba48d0ba79e4cf4ba8c025c468edef9e8
Bradley Woodcock <BradWoodcock00@gmail.com>
2022-11-16 13:34:34 -0500

    finished 1-5

--- script.js
+++ script.js
@@ -22,14 +22,45 @@ for (let i = 0; i < relevantDOMElements.length; i++) {
 // get a ref to each div in DOM
 relevantDOMElements = document.getElementsByTagName("div");
 Array.from(relevantDOMElements).forEach((divElem, i) => {
-console.log(`i: ${i} relevantDOMElements[i]: ${relevantDOMElements[i].innerText}`);
+//console.log(`i: ${i} relevantDOMElements[i]: ${relevantDOMElements[i].innerText}`);
+divElem.addEventListener("click", (e) =>
+  console.log(divElem.className + " was clicked!"));
 })
 
 // add event listener to each div
 
-// 3. element with id "donut" is clicked, log the number of words in the element
 
+// 3. element with id "donut" is clicked, log the number of words in the element
+relevantDOMElements = document.getElementById("donut");
+Array.from(relevantDOMElements).forEach((divElem, i) => {
+//console.log(`i: ${i} relevantDOMElements[i]: ${relevantDOMElements[i].innerText}`);
+divElem.addEventListener("click", (e) =>
+  console.log(divElem.className + " was clicked!")); 
+})
 
 // 4. when aside clicked, window.prompt then add id with content from prompt to aside's parent, log target
-// 5. add an input with classname as the first child of preceding id's first aside
+const aside = document.querySelector("body aside");
+aside.addEventListener("click", (e) => {
+  const userEnteredId = window.prompt("Enter an id:");
+  console.log(`userEnteredId: ${userEnteredId}`);
+  const parent = aside.parentElement;
+  parent.id = userEnteredId;
+  console.log(`e.target = ${e.target}`);
+
+  // 5. add an input with classname as the first child of preceding id's first aside
+const pattern5Result = document.querySelector(`#${userEnteredId} aside`);
+const newInputElem = document.createElement("input");
+newInputElem.type = "text";
+newInputElem.className = "best-class-og-12324"
+pattern5Result.prepend(newInputElem);
+
 // 6. log cowsay for the input
+const inputElementByClassname = document.querySelector(".best-class-og-12324");
+inputElementByClassname.addEventListener("click", () => {
+  const cowsay = fetch("https://helloacm.com/api/cowsay/?msg=CowSay")
+  .then(console.log(cowsay));
+  });
+})
+
+
+
